610d4714ad076422b6e869aebfd8d42cefcc78a3
euro <phuchit@omg.network>
2021-01-26 11:11:50 +0700

    Added commits messages to latest activities'

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,11 +5,12 @@ const moment = require('moment')
 function getActivities() {
  return axios.get("https://api.github.com/users/ripzery/events/public")
     .then((resp) => resp.data)
-    .then(resp => resp.slice(0, 10))
+    .then(resp => resp.slice(0, 5))
     .then(data => data.map(activity => ({
       id: activity.id,
       type: activity.type,
       repo: activity.repo,
+      payload: activity.payload,
       created_at: moment(activity.created_at).format('lll')
     })))
 }
--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,8 +1,47 @@
 import React from 'react'
-import { FaStar } from 'react-icons/fa'
+import { FaStar, FaChevronRight } from 'react-icons/fa'
 import "../styles/activity.css"
 
 function Activity({ activity }) {
+
+  function rewordedType() {
+    switch(activity.type){
+      case 'PushEvent':
+        return 'Pushed'
+      case 'WatchEvent':
+        return 'Watched'
+      case 'PublicEvent':
+        return 'Publiced'
+      default:
+        return activity.type
+    }
+  }
+
+  function renderPayload() {
+    if(activity.payload) {
+      if(activity.payload.commits) {
+        return (
+          <div className='activity-payload'>
+            <p className='activity-payload-header'>Commits:
+            </p>
+            <ul className='activity-payload-commits'>
+              {activity.payload.commits.map(({ message }) => {
+              return (
+                <li className='activity-payload-commits-item'>
+                  <div className='activity-payload-commits-item-icon'><FaChevronRight /></div>
+                  {message}
+                </li>
+              )
+              })}
+            </ul>
+          </div>
+          )
+      }
+    }
+
+    return null
+  }
+
   return (
     <div className='activity-container' id={activity.id}>
       <div className='activity-left'>
@@ -10,7 +49,8 @@ function Activity({ activity }) {
       </div>
       <div className='activity-right'>
         <p className='activity-time'>{activity.created_at}</p>
-        <p className='activity-event'>{activity.type} for {activity.repo.name}</p>
+        <p className='activity-event'>{rewordedType()} for {activity.repo.name}</p>
+        {renderPayload()}
       </div>
     </div>
   )
