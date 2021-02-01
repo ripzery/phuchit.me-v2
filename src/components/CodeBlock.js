171630bd92b5a8eb75fcbfe68e4dffb264bac706
euro <ripzery@gmail.com>
2021-02-01 18:28:05 +0700

    fix style in post

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -13,7 +13,7 @@ export default ({ children, className }) => {
     return (
     <Highlight {...defaultProps} code={children} language={language}>
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{style, marginTop: '1rem' }}>
+        <pre className={className} style={{style }}>
             {tokens.slice(0, tokens.length - 1).map((line, i) => (
             <div {...getLineProps({ line, key: i })}>
                 {line.map((token, key) => (
